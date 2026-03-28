88b8bfc958f514e3780b2c99770d11a65384b187
michal-ry <ryziomichal@gmail.com>
2026-03-28 20:26:31 +0100

    Add examples for mean

---
+++ examples/statistics/descriptive/mean.py
@@ -0,0 +1,20 @@
+from ml_from_scratch.statistics.descriptive.descriptive import mean as fs_mean
+import numpy as np
+
+
+def mean_example():
+
+    '''
+    Compare mean calculated from scratch with NumPy
+    '''
+
+    numbers = [3, 3, 4, 50]
+
+    from_scratch_mean = fs_mean(numbers)
+    numpy_mean = np.mean(numbers)
+
+    print(f'Mean using function from scratch: {from_scratch_mean}')
+    print(f'Mean using numpy: {numpy_mean}')
+
+if __name__ == "__main__":
+    mean_example()
